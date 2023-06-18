234ee3d689f40bd34f4957f2af6d61f70cb5b816
Marcel Weber <mrweber@gmx.ch>
2023-06-18 15:25:34 +0200

    Created page Angebote

---
+++ pages/angebote.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export default function Angebote() {
+  
+  return (
+    <main className="main">
+      <section className="section">
+        <h1 className="title">Angebote</h1>
+        
+      </section>
+    </main>
+  );
+}
