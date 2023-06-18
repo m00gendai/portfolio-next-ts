a44f8ff83c5b04f5245b5d47987ab74f6638c5fa
Marcel Weber <mrweber@gmx.ch>
2023-06-18 17:15:02 +0200

    Displaying offer components from API source implemented

--- components/Offer.tsx
+++ components/Offer.tsx
@@ -1,85 +1,49 @@
 import s from "../styles/Offer.module.css"
 
-export default function Offer(){
+interface Offer_part{
+    title: string;
+    description: string;
+    content: string;
+  }
+  
+  interface Offer_item{
+    title: string;
+    tagline: string;
+    price: string;
+    intro: string;
+    parts: Offer_part[];
+    _modified: number;
+    _mby: string;
+    _created: number;
+    _state: number;
+    _cby: string;
+    _id: string;
+  }
+
+  interface OfferProps{
+    offer: Offer_item;
+  }
+
+export default function Offer({offer}:OfferProps){
+    console.log
     return(
         <details className={s.details}>
           <summary className={s.summary}>
-            <h2>WordPress Webseite</h2>
-            <p>Schnell erstellt fürs kleine Budget</p>
-            <div className={s.price}>ab CHF 500</div>
+            <h2>{offer.title}</h2>
+            <p>{offer.tagline}</p>
+            <div className={s.price}>{offer.price}</div>
           </summary>
           <article className={s.description}>
-            WordPress Webseiten sind eine gute Lösung für kleinere Webseiten, die schnell erstellt sind und auch von Laien 
-            gewartet, angepasst und unterhalten werden können.
-            <h4>WordPress Webseite einrichten</h4>
-            <ul>
-                <li>WordPress auf der gewünschten Umgebung installieren</li>
-                <li>Systemeinstellungen vornehmen</li>
-                <li>Notwendige Plugins installieren & konfigurieren</li>
-                <li>Sicherheit der Webseite gewährleisten</li>
-            </ul>
-            <h4>Inhalte erstellen</h4>
-            <ul>
-                <li>Gewünschte Unterseiten erstellen</li>
-                <li>Texte einbinden (oder neu erstellen)</li>
-                <li>Bilder einbinden (oder via Bilderanbieter zusammensuchen)</li>
-            </ul>
-            <h4>Suchmaschinenoptimierung</h4>
-            <p>
-                Damit die Webseite bei Google und Konsorten in den Suchergebnissen vorne mit dabei ist, müssen einige Tricks und 
-                Kniffe angewandt werden. Viele erledigen das mit Plugins für WordPress - diese sind aber oftmals aufgeblasen und funktional eingeschränkt -
-                genau das, was man braucht, erfordert dann Premium-Versionen, die zeitweise sehr viel kosten können.
-                Ich erledige die Optimierung von Hand, nach neuesten Standards. Dazu gehört auch die korrekte Darstellung von Vorschaulinks zB auf sozialen Medien.
-            </p>
-            <ul>
-                <li>Sitemap erstellen</li>
-                <li>Webseite bei Suchmaschinen indexieren</li>
-                <li>Korrekte Semantik implementieren</li>
-                <li>Korrekte Vorschaulinks für soziale Medien prüfen</li>
-                <li>Für Barrierefreiheit sorgen</li>
-                <li>Webseitenchecks (Lighthouse) durchführen</li>
-                <li>Analytik & Statistik einbinden</li>
-            </ul>
-            <h4>Sicherheit</h4>
-            <p>
-                Dies ist ein Bereich, wo ich das Abdecken via Plugin empfehle. 
-                Es gibt viele gute Sicherheitsplugins, von gratis bis günstig.
-            </p>
-            <ul>
-                <li>Installieren & konfigurieren von Sicherheitsplugins</li>
-                <li>Zusätzliche Sicherheitsmassnahmen seitens Hoster vornehmen</li>
-            </ul>
-            <h4>Responsives Design</h4>
-            <p>
-                Die meisten Webseitenaufrufe kommen heutzutage von Mobilgeräten, dem muss man gerecht werden.
-                WordPress bietet dies in der Regel von Haus aus, allerdings müssen vor allem bei individuellen Elementen
-                die Einstellungen von Hand vorgenommen werden
-            </p>
-            <ul>
-                <li>Prüfen der Responsivität</li>
-                <li>Wo nötig Anpassungen vornehmen</li>
-            </ul>
-            <h4>Datenschutz</h4>
-            <p>
-                Die Umsetzung der europäischen Datenschutz Grundverordnung wird eingehalten - dies beinhaltet (leider) auch die nervigen Cookie-Meldungen. 
-                Diese werden von mir so wenig störend wie möglich implementiert.
-            </p>
-            <ul>
-                <li>EU-DSGVO umsetzen</li>
-                <li>Cookie-Meldung implementieren</li>
-                <li>Cookie-Handhabung implementieren</li>
-                <li>Datenschutzdeklaration erstellen</li>
-                <li>Impressum erstellen</li>
-            </ul>
-            <h4>Preis</h4>
-            <p>
-                Sofern Hosting und Domain bereits vorhanden sind, gilt der Preis von CHF500. 
-                Ansonsten kommt das Hosting und die Domain noch dazu. 
-                Der Gesamtpreis ist dann abhängig vom gewählten Anbieter.
-            </p>
-            <p>
-                In diesem Preis nicht inbegriffen sind etwaige kostenpflichtige Funktionen und Plugins.
-            </p>
+            <div dangerouslySetInnerHTML={{__html: offer.intro}} />
+            {offer.parts.map(part=>{
+                return(
+                    <div className={s.part}>
+                        <h4>{part.title}</h4>
+                        <div dangerouslySetInnerHTML={{__html: part.description}} />
+                        <div dangerouslySetInnerHTML={{__html: part.content}} />
+                    </div>
+                )
+            })}
           </article>
         </details>
     )
--- pages/angebote.tsx
+++ pages/angebote.tsx
@@ -1,14 +1,62 @@
 import * as React from 'react';
 import Offer from '@/components/Offer';
 
-export default function Angebote() {
-  
+interface Offer_part{
+  title: string;
+  description: string;
+  content: string;
+}
+
+interface Offer_item{
+  title: string;
+  tagline: string;
+  price: string;
+  intro: string;
+  parts: Offer_part[];
+  _modified: number;
+  _mby: string;
+  _created: number;
+  _state: number;
+  _cby: string;
+  _id: string;
+}
+
+interface OfferProps{
+  offers: Offer_item[]
+}
+
+export default function Angebote({offers}:OfferProps) {
+
   return (
     <main className="main">
       <section className="section">
       <h1 className="title">Angebote</h1>
-      <Offer />
+      {offers.map(offer=>{
+        return <Offer offer={offer} />
+      })}
       </section>
     </main>
   );
+}
+
+export async function getStaticProps(){
+
+  // the key is read-only so don't bother
+  const getOffers: Response = await fetch(
+    'https://cms.mrweber.ch/api/content/items/offer?populate=100',
+    {
+      headers: {
+        'api-key': 'API-773e67ee0ba102d8b93a74751560d8bdd07bd2cb',
+      },
+    }
+  )
+  
+  const offers:Offer_item[] = await getOffers.json()
+
+
+return{
+  props:{
+      offers
+  }, revalidate: 10
+}
 }
