176dc779a335a7e856a779257b5a613d0168922f
Marcel Weber <mrweber@gmx.ch>
2023-06-18 16:23:58 +0200

    Embedded Offer component atm just one component for dev purposes only

--- pages/angebote.tsx
+++ pages/angebote.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
+import Offer from '@/components/Offer';
 
 export default function Angebote() {
   
   return (
     <main className="main">
       <section className="section">
-        <h1 className="title">Angebote</h1>
-        
+      <h1 className="title">Angebote</h1>
+      <Offer />
       </section>
     </main>
   );
