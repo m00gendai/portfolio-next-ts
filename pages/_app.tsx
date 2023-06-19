bef4cc85197cd1434af77fa1af9e626b06f322a7
Marcel Weber <mrweber@gmx.ch>
2023-06-20 01:36:26 +0200

    useMediaQuery hook on app level

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useMediaQuery } from '@react-hook/media-query'
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const isMobile:boolean = useMediaQuery('only screen and (max-aspect-ratio: 13/9)')
+
   return (
   <>
   <Navbar />
-  <Component {...pageProps} />
+  <Component {...pageProps} isMobile={isMobile}/>
   </>
   )
 }
