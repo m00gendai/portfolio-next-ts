b9e207b3c7e20d67947459a2dc015a573ffe6e21
Marcel Weber <mrweber@gmx.ch>
2023-06-25 16:09:16 +0200

    Changed text animation Added CTA buttons Added background image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
 import Head from 'next/head'
 import {useState, useEffect} from "react"
 import s from "@/styles/Home.module.css"
+import Link from "next/link"
 
 export default function Home() {
 
   const prefixes:string[] = ["Vereins", "Portfolio", "Club", "Betriebs", "Info", "Geschäfts", "Hobby", "Projekt"]
   const [prefix, setPrefix] = useState<String>("Vereins")
   const [prefixIndex, setPrefixIndex] = useState<number>(1)
+  const [addAnimate, setAddAnimate] = useState<boolean>(false)
 
   useEffect(()=>{
     const interval:NodeJS.Timer = setInterval(function(){
-      console.log(prefixIndex, prefixes.length-1)
+      
       if(prefixIndex === prefixes.length-1){
         
         setPrefixIndex(0)
@@ -19,8 +21,16 @@ export default function Home() {
       }
       setPrefix(prefixes[prefixIndex])
     },5000)
+
     return () => clearInterval(interval)
   },[prefixIndex])
+
+  const background:React.CSSProperties = {
+    backgroundImage: `url("/laptop-ga99e6e23c_1920.jpg")`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+  }
 
 
   return (
@@ -32,13 +42,25 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="main">
-        <section className="section">
+        <section className="section" style={background}>
           <div className={s.splashText}>
             <span className={s.span}>{`Ihre neue `}</span>
             <div className={s.wrapper}>
-              <span className={`${s.spanL} ${s.span}`}>{prefix}</span>
+              <span className={`${s.spanL} ${s.span} ${s.animate }` }>{prefix}</span>
               <span className={`${s.spanR} ${s.span}`}>{`webseite?`}</span>
             </div>
+            <div className="buttonContainer">
+            <Link href="/angebote" className="buttonBg">
+              <div className="button">
+                Angebote
+              </div>
+            </Link>
+            <Link href="/portfolio" className="buttonBg">
+              <div className="button">
+                Portfolio
+              </div>
+            </Link>
+          </div>
           </div>
         </section>
       </main>
