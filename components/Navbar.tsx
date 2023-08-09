cad5811645386b0efd232dc7072f7e14aa20c03d
Marcel Weber <mrweber@gmx.ch>
2023-08-09 22:17:42 +0200

    Added Artikel link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,6 +9,7 @@ export default function Navbar(){
                 <Link className={s.link} href= "/portfolio" title="Portfolio">Portfolio</Link>
                 <Link className={s.link} href= "/angebote" title="Angebote">Angebote</Link>
                 <Link className={s.link} href= "/informationen" title="Informationen">Informationen</Link>
+                <Link className={s.link} href= "/artikel" title="Artikel">Artikel</Link>
                 <Link className={s.link} href= "/kontakt" title="Kontakt">Kontakt</Link>
             </div>
         </nav>
