083e76b8ffb6f441198692191846cdf641da954c
Marcel Weber <mrweber@gmx.ch>
2024-01-19 17:58:02 +0100

    FInalised generateMEtadata Cleaned up code

--- app/page.tsx
+++ app/page.tsx
@@ -1,78 +1,62 @@
 
 import SplashText from "@/components/SplashText";
 import s from "../styles/Home.module.css"
-
-interface tagline{
-    page: string;
-    title: string;
-    description: string;
-    _modified: number;
-    _mby: string;
-    _created: number;
-    _state: number;
-    _cby: string;
-    _id: string;
-  }
+import { Metadata } from "@/interfaces";
   
 interface Props{
-   params:{
-    slug: string
-   }
+    params:{
+        slug: string
+    }
 }
 
-  export async function generateMetadata({params}:Props){
-    const page:string = params.slug
-      const getTaglines: Response = await fetch(
-            `https://cms.mrweber.ch/api/content/item/taglines?filter=%7Bpage%3A%22${page === "" ? "Home" : page}%22%7D&populate=1`,
-            {
-              headers: {
+export async function generateMetadata({params}:Props){
+    const pageName:string = params.slug
+    const getMetadata: Response = await fetch(
+        `https://cms.mrweber.ch/api/content/item/taglines?filter=%7Bpage%3A%22${pageName === undefined ? "Home" : pageName}%22%7D&populate=1`,
+        {
+            headers: {
                 'api-key': `${process.env.COCKPIT}`,
-              },
-            }
-          )
-          
-          const taglines:tagline = await getTaglines.json()
-          return {
-            title: taglines.title,
-            description: taglines.description,
-            openGraph: {
-                title: taglines.title,
-                description: taglines.description,
-                images: [
-                    {
-                        url: taglines.page,
-                    }
-                ]
-            }
-          }
+            },
         }
+    )
+    const metadata:Metadata = await getMetadata.json()
 
-export default async function Home({params}:Props){
+    return {
+        title: metadata.title,
+        description: metadata.description,
+        openGraph: {
+            title: metadata.title,
+            description: metadata.description,
+            images: [
+                {
+                    url: `https://cms.mrweber.ch/storage/uploads/${metadata.image.path}`,
+                }
+            ]
+        }
+    }
+}
 
-       
-    
-      const background:React.CSSProperties = {
+export default async function Home(){
+    const background:React.CSSProperties = {
         backgroundImage: `url("/laptop-ga99e6e23c_1920.jpg")`,
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center",
-      }
+    }
     
-      const ctaText: string = `Überzeugen Sie sich von meinem <a href="/portfolio" title="zum Portfolio">Portfolio</a> und <a href="/kontakt" title="zum Kontaktformular">kontaktieren</a> Sie mich!`
+    const ctaText: string = `Überzeugen Sie sich von meinem <a href="/portfolio" title="zum Portfolio">Portfolio</a> und <a href="/kontakt" title="zum Kontaktformular">kontaktieren</a> Sie mich!`
     
-
     return(
-    <main className="homeMain">
-        <section className="homeSection" style={background}>
-          
-          <SplashText />
-        </section>
-        <span className={s.whackyWavingInflatableArmsFlailingTubeMan}>
-          <p>{`Ansprechende Webseiten aus regionalem Anbau für Vereine, Organisationen, Persönlichkeiten, KMUs und vieles mehr.`}</p>
-          <p>{`mrweber.ch ist nicht nur Webdesign, sondern auch Webentwicklung.` }</p>
-          <p>{`Von WordPress über ClubDesk bis zur handgemachten Massarbeit mit JavaScript/TypeScript, React und Next.js.`}</p>
-          <p dangerouslySetInnerHTML={{__html: ctaText}}></p>
-        </span>
-      </main>
+        <main className="homeMain">
+            <section className="homeSection" style={background}>
+                <SplashText />
+            </section>
+            <span className={s.whackyWavingInflatableArmsFlailingTubeMan}>
+                <p>{`Ansprechende Webseiten aus regionalem Anbau für Vereine, Organisationen, Persönlichkeiten, KMUs und vieles mehr.`}</p>
+                <p>{`mrweber.ch ist nicht nur Webdesign, sondern auch Webentwicklung.` }</p>
+                <p>{`Von WordPress über ClubDesk bis zur handgemachten Massarbeit mit JavaScript/TypeScript, React und Next.js.`}</p>
+                <p dangerouslySetInnerHTML={{__html: ctaText}}></p>
+            </span>
+        </main>
     )
 }
