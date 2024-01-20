a6752e0c72d6d5ac151a16a777fb589b47a1dce9
Marcel Weber <mrweber@gmx.ch>
2024-01-20 16:52:51 +0100

    Ported to client component

--- components/TechStack.tsx
+++ components/TechStack.tsx
@@ -1,44 +1,15 @@
+"use client"
+
 import Link from "next/link";
 import s from "../styles/TechStack.module.css"
 import d from "../styles/dialog.module.css"
 import { useRef, useState } from "react";
 import { BiLinkExternal, BiXCircle } from "react-icons/bi";
 import Image from "next/image"
-
-interface TechStack_image{
-    path: string;
-    title: string;
-    mime: string;
-    type: string;
-    description: string;
-    tags: string[];
-    size: number;
-    colors: string[];
-    width: number;
-    height: number;
-    _hash: string;
-    _created: number;
-    _modified: number;
-    _cby: string;
-    folder: string;
-    _id: string;
-}
-interface TechStack{
-    brand: string;
-    url: string;
-    excerpt: string;
-    image: TechStack_image;
-    Bildquelle: string;
-    _modified: number;
-    _mby: string;
-    _created: number;
-    _state: number;
-    _cby: string;
-    _id: string;
-}
+import { Stack } from "@/interfaces";
 
 interface TechProps{
-    tech: TechStack;
+    tech: Stack;
 }
 
 export default function TechStack({tech}:TechProps){
@@ -59,27 +30,31 @@ export default function TechStack({tech}:TechProps){
       
     return(
         <div className={s.card}>
-            <div 
-                className={s.inner} 
-                style={{backgroundImage: `url("https://cms.mrweber.ch/storage/uploads/${tech.image.path}")`,}}
-                onClick={()=>handleOpen()}
-            >
+            <div className={s.inner} onClick={()=>handleOpen()}>
+                <Image 
+                    src={`https://cms.mrweber.ch/storage/uploads/${tech.image.path}`}
+                    alt={tech.brand}
+                    fill={true}
+                    style={{objectFit: "contain"}}
+                    title={tech.brand}
+                />
             </div>
             <dialog className={d.container} ref={dialog}>
                 <div className={d.inner}>
                     <button className={d.button} onClick={()=>handleClose()}><BiXCircle /></button>
                     <h2 className={d.title}><Link className={d.link} href={tech.url} target="_blank" title={tech.brand}>{tech.brand}</Link><BiLinkExternal style={{margin: "0 0 0 0.5rem"}}/></h2>
                     <div className={d.grid}>
-                        <Image 
-                            className={d.image}
-                            src={`https://cms.mrweber.ch/storage/uploads/${tech.image.path}`}
-                            alt={tech.brand}
-                            width={tech.image.width === null ? 240 : tech.image.width}
-                            height={tech.image.height === null ? 240 : tech.image.height}
-                            style={imageStyle}
-                        >
-                        </Image>
-                        <div dangerouslySetInnerHTML={{__html: tech.excerpt}}>
+                        <div className={d.imageContainer}>
+                            <Image 
+                                className={d.image}
+                                src={`https://cms.mrweber.ch/storage/uploads/${tech.image.path}`}
+                                alt={tech.brand}
+                                fill={true}
+                                style={{objectFit: "contain"}}
+                            >
+                            </Image>
+                        </div>
+                        <div className={d.text} dangerouslySetInnerHTML={{__html: tech.excerpt}}>
                         </div>
                     </div>
                     <p className={d.notice}>Die Beschreibungen wurden mit ChatGPT erstellt.</p>
