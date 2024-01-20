df06c1ad0a9fc9937e73968279d1722a59f2c7a5
Marcel Weber <mrweber@gmx.ch>
2024-01-20 18:12:47 +0100

    Updated interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -154,3 +154,29 @@ export interface Stack{
   _cby: string;
   _id: string;
 }
+
+export interface isFocus {
+  name: boolean;
+  mail: boolean;
+  subject: boolean;
+  message: boolean;
+}
+
+export interface isFormValue {
+  name: string;
+  mail: string;
+  subject: string;
+  message: string;
+}
+
+export interface isFormValid {
+  name: boolean;
+  mail: boolean;
+  subject: boolean
+  message: boolean;
+}
+
+export interface isFeedback {
+  color: string;
+  content: string;
+}
